f0aea8de2bf48f55a31fbc4d8efbf44d2d6b6d71
Akash kumar Singh <lengendmaster7462@gmail.com>
2026-04-22 15:14:43 +0530

    Update Protfolio.js

--- Protfolio.js
+++ Protfolio.js
@@ -4,8 +4,17 @@ let navbarmenu = document.querySelector('.navbar-menu');
 navbartoggle.addEventListener('click', () => {
     navbartoggle.classList.toggle('active');
     navbarmenu.classList.toggle('active');
-})
+});
+if ('scrollRestoration' in history) {
+    history.scrollRestoration = 'manual';
+  }
 
   window.onload = function () {
     window.scrollTo(0, 0);
+  };
+   window.onload = function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
   };
